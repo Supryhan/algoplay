dc7dbec97be38bea95ff6d2b91b4cd90c31e55f0
Vitalii Supryhan <suprigan@gmail.com>
2016-11-10 01:02:27 +0200

    vary test

--- src/test/scala/StackTest.scala
+++ src/test/scala/StackTest.scala
@@ -5,7 +5,10 @@ class StackTest extends FunSuite {
   test("I can store three integers") {
     val stack = Cons(1, Cons(2, Cons(3, Empty)))
     val stack2 = Empty.cons(3).cons(2).cons(1)
+    val stack3 = Empty.cons(Empty).cons(Empty).cons(Empty)
+    val stack4 = Cons(Empty,Cons(Empty,Cons(Empty,Empty)))
     assert(stack == stack2)
+    assert(stack4 == stack3)
   }
 
   test("I can check head") {
