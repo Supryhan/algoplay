20a2f109f27749f4585990dbd177f8b5fa67465c
Vitalii Supryhan <suprigan@gmail.com>
2016-11-10 00:48:33 +0200

    investigate inheritance

--- src/test/scala/StackTest.scala
+++ src/test/scala/StackTest.scala
@@ -4,7 +4,7 @@ class StackTest extends FunSuite {
 
   test("I can store three integers") {
     val stack = Cons(1, Cons(2, Cons(3, Empty)))
-    val stack2 = Cons(3, Empty).cons(2).cons(1)
+    val stack2 = Empty.cons(3).cons(2).cons(1)
     assert(stack == stack2)
   }
 
