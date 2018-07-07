c84ff5e2a931bf1d22bbca08c2f9303ad620086e
Vitalii Supryhan <vsupryhan@playtika.com>
2018-07-07 14:33:51 +0300

    Merge remote-tracking branch 'remotes/origin/master'

--- src/main/scala/implicits/FBoundedPolymorphism.scala
+++ src/main/scala/implicits/FBoundedPolymorphism.scala
@@ -1,25 +1,25 @@
 package implicits
 
+sealed trait RootOps[A <: RootOps[A]] {
+  self: A =>
+  def getSymbol(implicit symbol: Symbol[A]): Symbol[A] = symbol
+}
+
+trait Alfa extends RootOps[Alfa]
+
+trait Beta extends RootOps[Beta]
+
+trait Gamma extends RootOps[Gamma]
+
+case object Alfa extends Alfa
+
+case object Beta extends Beta
+
+case object Gamma extends Gamma
+
+case class Symbol[A <: RootOps[A]](name: String)
+
 object FBoundedPolymorphism extends App {
-
-  sealed trait RootOps[A <: RootOps[A]] {
-    self: A =>
-    def getSymbol(implicit symbol: Symbol[A]): Symbol[A] = symbol
-  }
-
-  trait Alfa extends RootOps[Alfa]
-
-  trait Beta extends RootOps[Beta]
-
-  trait Gamma extends RootOps[Gamma]
-
-  case object Alfa extends Alfa
-
-  case object Beta extends Beta
-
-  case object Gamma extends Gamma
-
-  case class Symbol[A <: RootOps[A]](name: String)
 
   implicit val alfaSymbol = Symbol[Alfa]("alfa")
   implicit val betaSymbol = Symbol[Beta]("beta")
