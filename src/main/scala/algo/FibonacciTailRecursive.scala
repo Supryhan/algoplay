0ca96778951f2e890f7c58a4f9e8cc779d620949
Vitalii Supryhan <suprigan@gmail.com>
2018-07-09 00:52:45 +0300

    Merge remote-tracking branch 'remotes/origin/master'

--- src/main/scala/algo/FibonacciTailRecursive.scala
+++ src/main/scala/algo/FibonacciTailRecursive.scala
@@ -4,7 +4,7 @@ import scala.annotation.tailrec
 
 object FibonacciTailRecursive extends App {
 
-  println(fib(9))
+  println(fib(8))//1 1 2 3 5 8 13 21 34 55 89 144
 
   def fib(x: Int): BigInt = {
     @tailrec def fibHelper(x: Int, prev: BigInt = 0, next: BigInt = 1): BigInt = x match {
