e68412fe6d4e36f9a3dcaf2ebda2106ad9bbc8f4
Vitalii Supryhan <suprigan@gmail.com>
2018-07-12 00:21:28 +0300

    Merge remote-tracking branch 'remotes/origin/master'

---
+++ src/main/scala/implicits/ImplicitTuple.scala
@@ -0,0 +1,23 @@
+package implicits
+
+class ImplicitTuple extends App {
+  Show.show(12 -> "34") // "(12, 34)"
+}
+
+trait Show[T] {
+
+  def show(value: T): String
+}
+
+object Show {
+
+  def apply[T: Show]: Show[T] = implicitly[Show[T]]
+
+  def show[T: Show](value: T): String = apply[T].show(value)
+
+  implicit val showString: Show[String] = s => s
+  implicit val showInt: Show[Int] = _.toString
+
+  implicit def showTuple[T: Show, U: Show]: Show[(T, U)] =
+    tuple => s"(${Show.show[T](tuple._1)}, ${Show.show[U](tuple._2)})"
+}
