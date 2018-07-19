27389a95f45c36ac3e6d6a36d7e02da45f0df2a7
Vitalii Supryhan <suprigan@gmail.com>
2018-07-19 21:36:50 +0300

    Merge branch 'master' of https://github.com/Supryhan/algoplay
    # Conflicts:
    #	src/main/scala/implicits/ImplicitTuple.scala

--- src/main/scala/implicits/ImplicitTuple.scala
+++ src/main/scala/implicits/ImplicitTuple.scala
@@ -1,7 +1,9 @@
 package implicits
 
 object ImplicitTuple extends App {
-  print(Show.show(12 -> "34")) // "(12, 34)"
+  println(Show.showArgument(12 -> "34")) // "(12, 34)"
+  println(Show.showArgument("34")) // "(12, 34)"
+  print(Show.showArgument(12)) // "(12, 34)"
 }
 
 trait Show[T] {
@@ -11,16 +13,24 @@ trait Show[T] {
 
 object Show {
 
-    def apply[TT: Show](): Show[TT] = implicitly[Show[TT]]
+  def apply[TT: Show]: Show[TT] = implicitly[Show[TT]]
 
-  def show[T: Show](value: T): String = apply[T].show(value)
+  def showArgument[T: Show](value: T): String = apply[T].show(value)
 
-    implicit val showString: Show[String] = s => s
-//  implicit val showString = new Show[String] {
-//    def show(value: String): String = value
-//  }
-  implicit val showInt: Show[Int] = _.toString
+  implicit val string2Show: Show[String] = s => s
+  //  implicit val string2Show: Show[String] = new Show[String] {
+  //    def show(value: String): String = value
+  //  }
 
-  implicit def showTuple[T: Show, U: Show]: Show[(T, U)] =
-    argument => s"(${Show.show[T](argument._1)}, ${Show.show[U](argument._2)})"
+  implicit val int2Show: Show[Int] = _.toString
+  //  implicit val int2Show: Show[Int] = new Show[Int] {
+  //    override def show(value: Int): String = value.toString
+  //  }
+
+  implicit def tuple2Show[T: Show, U: Show]: Show[(T, U)] =
+    tuple => s"(${Show.showArgument[T](tuple._1)}, ${Show.showArgument[U](tuple._2)})"
+
+  //  implicit def tuple2Show[T: Show, U: Show]: Show[(T, U)] = new Show[(T, U)]{
+  //    override def show(tuple: (T, U)): String = s"(${Show.showArgument[T](tuple._1)}, ${Show.showArgument[U](tuple._2)})"
+  //  }
 }
