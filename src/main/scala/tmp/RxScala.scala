7231bd6c8653e96fd5c0d017b0ead7843d201b16
Vitalii Supryhan <suprigan@gmail.com>
2018-09-29 15:40:22 +0300

    Merge branch 'master' of https://github.com/Supryhan/algoplay
    # Conflicts:
    #	src/main/scala/implicits/ImplicitTuple.scala

--- src/main/scala/tmp/RxScala.scala
+++ src/main/scala/tmp/RxScala.scala
@@ -1,5 +1,16 @@
 package tmp
 
-object RxScala extends App{
+object RxScala extends App {
+  def foldRightUsingFoldLeft[A, B](seq: Seq[A], z: B)(f: (A, B) => B): B = foldLeft(seq.reverse, z)((b, a) => f(a, b))
 
+  def foldLeft[A, B](seq: Seq[A], z: B)(f: (B, A) => B): B =
+    seq match {
+      case Nil => z
+      case x :: xs => foldLeft(xs, f(z, x))(f)
+    }
+
+  val list: Seq[Int] = List(1, 2, 3, 4, 5, 6, 7)
+  val sum = foldRightUsingFoldLeft(list, "") (_ + _)
+  println(sum)
+  print(list.toString())
 }
