b435836901d326f29892c7d00af377787907326a
Vitalii Supryhan <suprigan@gmail.com>
2019-01-01 12:22:47 +0200

    add implicit converssion when dealing with AnyVal

--- src/main/scala/implicits/MonoidSyntax.scala
+++ src/main/scala/implicits/MonoidSyntax.scala
@@ -4,33 +4,32 @@ object MonoidSyntax extends App {
 
   trait Monoid[A] {
     def empty: A
-
     def append(a1: A, a2: A): A
-
     def concat(list: List[A]): A = list.foldRight(empty)(append)
   }
 
-  implicit val intMonoid = new Monoid[Int] {
-    def empty: Int = 0
-
-    def append(a1: Int, a2: Int): Int = a1 + a2
-  }
-  implicit val boolMonoid = new Monoid[Boolean] {
-    def empty: Boolean = false
-
-    def append(a1: Boolean, a2: Boolean): Boolean = a1.&&(a2)
+  object Monoid {
+    implicit val IntMonoid: Monoid[Int] = new Monoid[Int] {
+      def empty: Int = 0
+      def append(a1: Int, a2: Int): Int = a1 + a2
+    }
+    implicit val BoolMonoid: Monoid[Boolean] = new Monoid[Boolean] {
+      def empty: Boolean = false
+      def append(a1: Boolean, a2: Boolean): Boolean = a1.&&(a2)
+    }
   }
 
   trait Show[A] {
     def show(a: A): String
   }
 
-  implicit val intShow = new Show[Int] {
-    def show(a: Int): String = a.toString
-  }
-
-  implicit val boolShow = new Show[Boolean] {
-    def show(a: Boolean): String = String.valueOf(a)
+  object Show {
+    implicit val IntShow: Show[Int] = new Show[Int] {
+      def show(a: Int): String = a.toString
+    }
+    implicit val BoolShow: Show[Boolean] = new Show[Boolean] {
+      def show(a: Boolean): String = String.valueOf(a)
+    }
   }
 
   def combineValues[T](map: Map[String, List[T]])(monoid: Monoid[T]): Map[String, T] = map.mapValues(monoid.concat)
@@ -45,6 +44,6 @@ object MonoidSyntax extends App {
 
   def addAndShow[A: Monoid : Show](a1: A, a2: A): String = (a1 |+| a2).show
 
-    println(addAndShow(true, false))
-    println(addAndShow(1, 2))
+  println(addAndShow(true, false))
+  println(addAndShow(1, 2))
 }
