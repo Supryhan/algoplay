95c8e77e54e7962b1132c8093c93a2182a636995
vitalii supryhan <suprigan@gmail.com>
2019-02-11 18:35:47 +0200

    add lens modify

---
+++ src/main/scala/lens/LensMonocleTrainer.scala
@@ -0,0 +1,44 @@
+package lens
+
+import monocle.Lens
+import monocle.macros.GenLens
+
+object LensTrainer extends App {
+  val initialPerson = Person("Mike", Address(1333, Street("KarlsPlaz")))
+  val personDirectCopy = initialPerson.copy(
+    address = initialPerson.address.copy(
+      street = initialPerson.address.street.copy(
+        name = "Sechshauserstrasse"
+      )
+    )
+  )
+  object Manual {
+    val streetManualLens = Lens[Street, String](_.name)(string => street => street.copy(name = string))
+    val addressManualLens = Lens[Address, Street](_.street)(street => address => address.copy(street = street))
+    val personManualLens = Lens[Person, Address](_.address)(address => person => person.copy(address = address))
+}
+  object Semi {
+      val streetSemiLens :Lens[Street, String] = GenLens[Street](_.name)
+      val addressSemiLens :Lens[Address, Street] = GenLens[Address](_.street)
+      val personSemiLens :Lens[Person, Address] = GenLens[Person](_.address)
+  }
+
+  import Manual._
+  val changeStreetNameManualLens = personManualLens composeLens addressManualLens composeLens streetManualLens
+  import Semi._
+  val changeStreetNameSemiLens = personSemiLens.composeLens(addressSemiLens).composeLens(streetSemiLens)
+
+  val personManualLensCopy = changeStreetNameManualLens.set("Leopoldstrasse")(initialPerson)
+  val personSemiLensCopy = changeStreetNameSemiLens.set("Mariastrasse")(initialPerson)
+
+  println(initialPerson)
+  println(personDirectCopy)
+  println(personManualLensCopy)
+  println(personSemiLensCopy)
+}
+
+case class Street(name: String)
+
+case class Address(number: Int, street: Street)
+
+case class Person(name: String, address: Address)
