8e17d016cd09db10a87c7b599d9e6eeb8947cf88
vitalii supryhan <suprigan@gmail.com>
2019-02-12 16:54:05 +0200

    refactor for sbt

--- build.sbt
+++ build.sbt
@@ -7,5 +7,9 @@ scalaVersion := "2.12.8"
 libraryDependencies ++= Seq(
   "com.github.julien-truffaut" %% "monocle-core" % "1.6.0-M1",
   "com.github.julien-truffaut" %% "monocle-macro" % "1.6.0-M1",
-  "org.scalatest" %% "scalatest" % "3.0.5" % Test
+  "org.scalacheck" %% "scalacheck" % "1.14.0" % Test,
+  "com.github.julien-truffaut" %% "monocle-generic" % "1.5.0",
+  "com.github.julien-truffaut" %% "monocle-refined" % "1.6.0-M1",
+  "org.scalatest" %% "scalatest" % "3.0.5" % Test,
+  "com.github.julien-truffaut" %% "monocle-law" % "1.6.0-M1" % Test
 )
--- src/test/scala/lens/ApplicationMonocleTest.scala
+++ src/test/scala/lens/ApplicationMonocleTest.scala
@@ -2,10 +2,12 @@ package lens
 
 import lens.ApplicationMonocle.Manual
 import org.scalatest.FunSuite
+import utils.MonocleSuite
 
 class ApplicationMonocleTest extends MonocleSuite {
-  val initialPerson = Person("Mike", Address(1333, Street("KarlsPlaz")))
-  test("_)") {
-//    Manual.personManualLens.get(initialPerson) shouldEqual initialPerson
+  val expectedPerson = Person("Mike", Address(1333, Street("KarlsPlaz")))
+  val expectedAddress = Address(1333, Street("KarlsPlaz"))
+  test("test manual personToAddress lens ") {
+    Manual.personManualLens.get(expectedPerson) shouldEqual expectedAddress
   }
 }
