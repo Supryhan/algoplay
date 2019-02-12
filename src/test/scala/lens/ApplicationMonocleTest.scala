94f20080ef317bd1ca6f7414d4fe1433079d84fc
vitalii supryhan <suprigan@gmail.com>
2019-02-12 18:37:04 +0200

    refactor for sbt

--- src/test/scala/lens/ApplicationMonocleTest.scala
+++ src/test/scala/lens/ApplicationMonocleTest.scala
@@ -1,9 +1,8 @@
 package lens
 
-import lens.ApplicationMonocle.Manual
+import lens.ApplicationMonocle.Manual.personManualLens
+import lens.ApplicationMonocle.Semi.personSemiLens
 import utils.MonocleSuite
-import Manual._
-
 class ApplicationMonocleTest extends MonocleSuite {
 
   test(
@@ -20,5 +19,21 @@ class ApplicationMonocleTest extends MonocleSuite {
     val person = Person("Mike", Address(1333, Street("KarlsPlaz")))
     monocle.law.LensLaws(personManualLens).getSet(person)
     personManualLens.set(personManualLens.get(person))(person) shouldEqual person
+  }
+
+  test(
+    "Semi Lens. " +
+      "Address should be set into person and then returns updated address") {
+    val person = Person("Mike", Address(1, Street("KarlsPlaz1")))
+    val address = Address(1333, Street("KarlsPlaz2"))
+    monocle.law.LensLaws(personSemiLens).setGet(person, address)
+    personSemiLens.get(personSemiLens.set(address)(person)) shouldEqual address
+  }
+
+  test("Semi Lens. " +
+    "Address should be get from person and then set into person and then returns updated person") {
+    val person = Person("Mike", Address(1333, Street("KarlsPlaz")))
+    monocle.law.LensLaws(personSemiLens).getSet(person)
+    personSemiLens.set(personSemiLens.get(person))(person) shouldEqual person
   }
 }
