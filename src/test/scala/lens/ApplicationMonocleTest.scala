34fa7558ff20b513d3f80160c70b0d8611235c64
vitalii supryhan <suprigan@gmail.com>
2019-02-12 18:20:00 +0200

    refactor for sbt

--- src/test/scala/lens/ApplicationMonocleTest.scala
+++ src/test/scala/lens/ApplicationMonocleTest.scala
@@ -1,13 +1,27 @@
 package lens
 
 import lens.ApplicationMonocle.Manual
-import org.scalatest.FunSuite
 import utils.MonocleSuite
+import Manual._
 
 class ApplicationMonocleTest extends MonocleSuite {
-  val expectedPerson = Person("Mike", Address(1333, Street("KarlsPlaz")))
-  val expectedAddress = Address(1333, Street("KarlsPlaz"))
+
   test("test manual personToAddress lens ") {
-    Manual.personManualLens.get(expectedPerson) shouldEqual expectedAddress
+    val person = Person("Mike", Address(1333, Street("KarlsPlaz")))
+    val address = Address(1333, Street("KarlsPlaz"))
+    personManualLens.get(person) shouldEqual address
+  }
+
+  test("address should be set into person and then returns the updated address") {
+    val person = Person("Mike", Address(1, Street("KarlsPlaz1")))
+    val address = Address(1333, Street("KarlsPlaz2"))
+    monocle.law.LensLaws(personManualLens).setGet(person, address)
+    personManualLens.get(personManualLens.set(address)(person)) shouldEqual address
+  }
+
+  test("address should be get from person and then returns") {
+    val person = Person("Mike", Address(1333, Street("KarlsPlaz")))
+    monocle.law.LensLaws(personManualLens).getSet(person)
+    personManualLens.set(personManualLens.get(person))(person) shouldEqual person
   }
 }
