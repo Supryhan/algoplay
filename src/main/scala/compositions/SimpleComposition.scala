ea306f882d7cef5701c2858a9781e013adc82ccb
Vitalii Supryhan <suprigan@gmail.com>
2019-03-01 22:24:18 +0200

    add flatMap-style processing

--- src/main/scala/compositions/SimpleComposition.scala
+++ src/main/scala/compositions/SimpleComposition.scala
@@ -10,28 +10,28 @@ object SimpleComposition {
 
   case class Bread()
 
-  def grind: (Wheat => Flour) = w => {
+  def grind: Wheat => Flour = w => {
     println("make the flour");
     Flour()
   }
 
-  def kneadDough: (Flour => Dough) = f => {
+  def kneadDough: Flour => Dough = f => {
     println("make the dough");
     Dough()
   }
 
-  def distributeDough: (Dough => Seq[Dough]) = d => {
+  def distributeDough: Dough => Seq[Dough] = d => {
     println("distribute the dough");
     Seq[Dough]()
   }
 
-  def bake: (Int => Int => Seq[Dough] => Seq[Bread]) =
+  def bake: Int => Int => Seq[Dough] => Seq[Bread] =
     temperature => duration => sd => {
       println(s"bake the bread, duration: $duration, temperature: $temperature")
       Seq[Bread]()
     }
 
-  def bakeRecipe1 = bake(350)(45)
+  def bakeRecipe1: Seq[Dough] => Seq[Bread] = bake(350)(45)
 
   def main(args: Array[String]): Unit = {
     (grind >> kneadDough >> distributeDough >> bakeRecipe1) (Wheat())
