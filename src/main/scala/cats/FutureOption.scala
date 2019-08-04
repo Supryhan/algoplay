703502584b0d79e30906aa8acf2998ae18b77ec4
Vitalii Supryhan <suprigan@gmail.com>
2019-08-04 23:53:26 +0300

    add flatMap-style processing

--- src/main/scala/cats/FutureOption.scala
+++ src/main/scala/cats/FutureOption.scala
@@ -1,5 +1,7 @@
 package cats
 
+import cats.FutureOption.{Address, User}
+import cats.Storage.users
 import cats.data.OptionT
 import cats.implicits._
 
@@ -8,17 +10,13 @@ import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutor, Futu
 
 object FutureOption extends App {
   implicit val ec: ExecutionContextExecutor = ExecutionContext.global
-  val users = Map(
-    1L -> User(1, "K1", Address("Addrr11")),
-    2L -> User(1, "K2", Address("Addrr22")),
-    3L -> User(1, "K3", Address("Addrr33")))
-  val result = Await.result(findAddressByUserId(3L).value, 1.millis)
+  val result = Await.result(findAddressByUserId(3).value, 1.millis)
   result match {
     case Some(address) => println(s"User's address is: ${address.name}")
     case None => println("There was an error")
   }
 
-  def findUserById(id: Long)(implicit e: ExecutionContext): Future[Option[User]] = {
+  def findUserById(id: Int)(implicit e: ExecutionContext): Future[Option[User]] = {
     users(id) match {
       case user: User => Future.successful(Some(user))
       case _ => Future.failed(new Exception("Wrong user's id"))
@@ -30,10 +28,9 @@ object FutureOption extends App {
       case address => Future.successful(Some(address))
       case _ => Future.failed(new Exception(s"Address for user with id = ${user.id} does not exist"))
     }
-
   }
 
-  def findAddressByUserId(id: Long)(implicit e: ExecutionContext): OptionT[Future, Address] =
+  def findAddressByUserId(id: Int)(implicit e: ExecutionContext): OptionT[Future, Address] =
     for {
       user <- OptionT(findUserById(id))
       address <- OptionT(findAddressByUser(user))
@@ -44,4 +41,11 @@ object FutureOption extends App {
   case class Address(name: String)
 
 
+}
+
+object Storage {
+  val users = Map(
+    1 -> User(1, "K1", Address("Addrr11")),
+    2 -> User(1, "K2", Address("Addrr22")),
+    3 -> User(1, "K3", Address("Addrr33")))
 }
