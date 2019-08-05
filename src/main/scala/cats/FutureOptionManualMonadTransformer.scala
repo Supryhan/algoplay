7ee19189169d8784abf5d1ec90c349f44667b6e4
Vitalii Supryhan <suprigan@gmail.com>
2019-08-05 12:17:36 +0300

    add flatMap-style processing

--- src/main/scala/cats/FutureOptionManualMonadTransformer.scala
+++ src/main/scala/cats/FutureOptionManualMonadTransformer.scala
@@ -1,19 +1,26 @@
 package cats
 
-import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
+import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutor, Future}
+import scala.concurrent.duration._
 
 object FutureOptionManualMonadTransformer extends App {
   implicit val ec: ExecutionContextExecutor = ExecutionContext.global
 
   def findUserById(id: Long): Future[Option[User]] = Future.successful(Some(new User))
 
-  def findAddressByUser(user: User) = Future.successful(Some(new Address))
+  def findAddressByUser(user: User) = Future.successful(Some(new Address("blvd")))
 
   def findAddressByUserId(id: Long): Future[Option[Address]] =
     (for {
       user <- FutOpt(findUserById(id))
       address <- FutOpt(findAddressByUser(user))
     } yield address).value
+
+  val result = Await.result(findAddressByUserId(13L), 1.millis)
+  result match {
+    case Some(address) => print(address.name)
+    case None => print("No address found")
+  }
 
   case class FutOpt[A](value: Future[Option[A]]) {
 
@@ -27,7 +34,7 @@ object FutureOptionManualMonadTransformer extends App {
       }))
   }
 
-  case class Address()
+  case class Address(name: String)
 
   case class User()
 
