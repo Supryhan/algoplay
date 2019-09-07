82e8760f58a38167ffc9664fcced9b9373e93e2e
Vitalii Supryhan <suprigan@gmail.com>
2019-09-07 20:08:25 +0300

    refactor mycats

--- src/main/scala/algo/FibonacciTailRecursive.scala
+++ src/main/scala/algo/FibonacciTailRecursive.scala
@@ -4,17 +4,18 @@ import scala.annotation.tailrec
 
 object FibonacciTailRecursive extends App {
 
-  println(8.fib)//1 1 2 3 5 8 13 21 34 55 89 144
-
+  println(8.fib)  //1 1 2 3 5 8 13 21 34 55 89 144
 
   implicit class Fib(value: Int) {
-  def fib: BigInt = {
-    @tailrec def fibHelper(x: Int, prev: BigInt = 0, next: BigInt = 1): BigInt = x match {
-      case 0 => prev
-      case 1 => next
-      case _ => fibHelper(x - 1, next, next + prev)
+    def fib: BigInt = {
+      @tailrec def fibHelper(x: Int, prev: BigInt = 0, next: BigInt = 1): BigInt = x match {
+        case 0 => prev
+        case 1 => next
+        case _ => fibHelper(x - 1, next, next + prev)
+      }
+
+      fibHelper(value)
     }
-    fibHelper(value)
-  }}
+  }
 
 }
