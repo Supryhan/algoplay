d9dcbb53a2b16c3d92f30d32bb4f6add620f89ee
Vitalii Supryhan <suprigan@gmail.com>
2019-12-15 22:02:16 +0200

    add cats Id[A]

--- src/main/scala/problems/JsonMapUtils.scala
+++ src/main/scala/problems/JsonMapUtils.scala
@@ -3,19 +3,30 @@ package problems
 import io.circe._, io.circe.parser._
 
 class JsonMapUtils {
-
 }
-
 
 object ElasticSearchProcessor extends App {
 
   val rawJson: String =
     """{
-    |     "foo": "bar",
-    |     "baz": 123,
-    |     "list of stuff": [ 4, 5, 6 ]
-    |   }""".stripMargin
-  val parseResult = parse(rawJson)
-  print(parseResult)
+      |     "foo": "bar",
+      |     "baz": 123,
+      |     "list of stuff": [ 4, 5, 6 ],
+      |     "trickyMap": [
+      |       {"key1": "value1"},
+      |       {"key2": {"innerName": "innerValue"}}
+      |     ]
+      |   }""".stripMargin
+  val parsedJson = parse(rawJson).getOrElse(Json.Null)
+  //  print(parsedJson)
+  val mapCursor = parsedJson.hcursor
+
+  print(mapCursor.downField("trickyMap").downArray.as[Json] match {
+    case Right(value) => value.hcursor.downField("key1").as[String] match {
+      case Right(value) => value
+      case Left(value) => value
+    }
+    case Left(value) => value
+  })
 
 }
