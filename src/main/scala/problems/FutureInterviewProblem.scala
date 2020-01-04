aac3df350b6e78d7e27d0014dc204fd722f72b4d
Vitalii Supryhan <suprigan@gmail.com>
2020-01-04 22:15:51 +0200

    add cats Id[A]

--- src/main/scala/problems/FutureInterviewProblem.scala
+++ src/main/scala/problems/FutureInterviewProblem.scala
@@ -2,12 +2,22 @@ package problems
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.{Success, Failure}
 
 object FutureInterviewProblem {
 
   type SimpleService[T] = () => Future[T]
 
-  def repeater[T](service: SimpleService[T], attempt: Int): Future[T] = ???
+  def repeater[T](service: SimpleService[T], attempt: Int): Future[T] = {
+    println(s"Attempt:$attempt")
+    val r = service()
+    r onComplete {
+      case Success(value) => value
+      case Failure(exception) if attempt > 1 => repeater(service, attempt - 1)
+      case Failure(exception) => exception
+    }
+    r
+  }
 
   def main(args: Array[String]): Unit = {
     def buggyService(): Future[String] =
@@ -17,6 +27,10 @@ object FutureInterviewProblem {
         if (Math.random() > 0.7) "Done" else sys.error("try again")
       }
 
-    repeater(buggyService _, 5)
+    repeater(buggyService _, 5) onComplete {
+      case Success(value) => print(value)
+      case Failure(exception) => print(exception)
+    }
+    Thread.sleep(2500)
   }
 }
