eb97459bdcaca2e5d1d7d49e8f8e6484e23391f6
supryhan <suprigan@gmail.com>
2020-10-30 23:34:18 +0200

    refactor CSP

--- src/main/scala/catslaboratory/CatsTraining.scala
+++ src/main/scala/catslaboratory/CatsTraining.scala
@@ -2,38 +2,34 @@ package catslaboratory
 
 object CatsTraining extends App {
 
-  trait Sem[A] {
-  }
-
-  trait Mon[A] extends Sem[A] {
-    def em: A
-
-    def \|+|/(a1: A, a2: A): A
-
-    def comb[A](a1: A, a2: A)(implicit m: Mon[A]): A = m.\|+|/(a1, a2)
-  }
-
-  object Monoid {
-    implicit val str: Mon[String] = new Mon[String] {
-      def \|+|/(a1: String, a2: String): String = s"$a1$em$a2"
-
-      def em: String = "->"
-    }
-
-    implicit val int: Mon[Int] = new Mon[Int] {
-      def \|+|/(a1: Int, a2: Int): Int = s"$a1$em$a2".toInt
-
-      def em: Int = 0
-    }
-  }
-
   implicit class C[A](val a: A) extends AnyVal {
-    def |+|(aa: A)(implicit monoid: Mon[A]): A = monoid.comb(a, aa)
+    def combineWith(aa: A)(implicit monoid: MonoidCT[A]): A = monoid.combine(a, aa)
   }
 
-  import Monoid._
+  import MonoidCT._
 
-  println("a1" |+| "b4")
-  println(1 |+| 4)
+  println("a1" combineWith "b4")
+  println(1 combineWith 4)
+}
 
+trait SemigroupCT[A] {
+  def combine(a1: A, a2: A): A
+}
+
+trait MonoidCT[A] extends SemigroupCT[A] {
+  def empty: A
+}
+
+object MonoidCT {
+  implicit val str: MonoidCT[String] = new MonoidCT[String] {
+    def empty: String = "->"
+
+    def combine(a1: String, a2: String): String = s"$a1$empty$a2"
+  }
+
+  implicit val int: MonoidCT[Int] = new MonoidCT[Int] {
+    def empty: Int = 0
+
+    def combine(a1: Int, a2: Int): Int = s"$a1$empty$a2".toInt
+  }
 }
