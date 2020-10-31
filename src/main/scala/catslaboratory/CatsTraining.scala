c94ba14490ce3068ab9312edf52120a08c9788c8
supryhan <suprigan@gmail.com>
2020-10-31 22:07:02 +0200

    refactor

--- src/main/scala/catslaboratory/CatsTraining.scala
+++ src/main/scala/catslaboratory/CatsTraining.scala
@@ -3,31 +3,29 @@ package catslaboratory
 object CatsTraining extends App {
 
   implicit class C[A](val a: A) extends AnyVal {
-    def combineWith(aa: A)(implicit monoid: MonoidCT[A]): A = monoid.combine(a, aa)
+    def combineWith(aa: A)(implicit monoid: Monoid[A]): A = monoid.combine(a, aa)
   }
-
-  import MonoidCT._
 
   println("a1" combineWith "b4")
   println(1 combineWith 4)
 }
 
-trait SemigroupCT[A] {
+trait Semigroup[A] {
   def combine(a1: A, a2: A): A
 }
 
-trait MonoidCT[A] extends SemigroupCT[A] {
+trait Monoid[A] extends Semigroup[A] {
   def empty: A
 }
 
-object MonoidCT {
-  implicit val str: MonoidCT[String] = new MonoidCT[String] {
+object Monoid {
+  implicit val str: Monoid[String] = new Monoid[String] {
     def empty: String = "->"
 
     def combine(a1: String, a2: String): String = s"$a1$empty$a2"
   }
 
-  implicit val int: MonoidCT[Int] = new MonoidCT[Int] {
+  implicit val int: Monoid[Int] = new Monoid[Int] {
     def empty: Int = 0
 
     def combine(a1: Int, a2: Int): Int = s"$a1$empty$a2".toInt
