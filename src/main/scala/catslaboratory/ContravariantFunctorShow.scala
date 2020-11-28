6d7aa65c96f0c1ce01a7ba41bb70462b255ad046
supryhan <suprigan@gmail.com>
2020-11-28 21:50:18 +0200

    refactor Show

--- src/main/scala/catslaboratory/ContravariantFunctorShow.scala
+++ src/main/scala/catslaboratory/ContravariantFunctorShow.scala
@@ -6,16 +6,17 @@ import cats.implicits._
 object ContravariantFunctorShow extends App {
 
 
-  implicit val showMoney: Show[Money] = Show.show(m => s"money: $$${m.amount}")
+  implicit val showMoney: Show[Money] = Show.show((m: Money) => s"money: $$${m.amount}")
 
                                                  //           Show[M] =>  (S => M)  => Show[S]
                                                  // contramap(value: F[A])(f: B => A): F[B]
   implicit val showSalary: Show[Salary] = showMoney.contramap[Salary](salary => salary.size)
 
-  println(Money(100500).show)
-  println(Salary(Money(1000)).show)
+  println("500".show)
+  println(Money("100").show)
+  println(Salary(Money("1000500")).show)
 }
 
-case class Money(amount: Int)
+case class Money(amount: String)
 
 case class Salary(size: Money)
