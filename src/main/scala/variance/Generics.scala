e1bcac637f19ca5bfed17f1876d6b597fc9f4233
supryhan <suprigan@gmail.com>
2020-12-20 23:21:19 +0200

    refactor

--- src/main/scala/variance/Generics.scala
+++ src/main/scala/variance/Generics.scala
@@ -1,12 +1,13 @@
 package variance
 
-object Generics{
+object Generics {
   def main(args: Array[String]) {
 
   }
 }
-class GenericCellMut[-T](val x: T){
-  def foo(x: T) = ???
+
+class GenericCellMut[+T](val x: T) {
+  def foo[U >: T](x: U): Unit = println()
 }
 
 //class Generics[-A, +B] {
@@ -17,6 +18,34 @@ class GenericCellMut[-T](val x: T){
 //  }
 //}
 
-class A {def a(){}}
-class B extends A {def b(){}}
-class C extends B {def c(){}}
+class A {
+  def a() {}
+}
+
+class B extends A {
+  def b() {}
+}
+
+class C extends B {
+  def c() {}
+}
+
+sealed abstract class Animal2
+case class Cat2() extends Animal2
+class Cat22() extends Cat2
+case class Dog2() extends Animal2
+
+case class Box2[+A](value: A) {
+  def set[B >: A](a: B): A = value
+}
+case class Box22[-A, +B](value: B) {
+  def set(a: A): B = value
+}
+
+object Box2 extends App {
+  val catBox = new Box2[Cat2](Cat2())
+  val animalBox: Box2[Animal2] = catBox
+  val dog = new Dog2
+  animalBox.set(dog)
+  Box2[Cat2](new Cat22()).set(new Animal2{})
+}
