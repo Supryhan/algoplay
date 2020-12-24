5ff7069c916fd598a54749a255d341c9093654a3
supryhan <suprigan@gmail.com>
2020-12-24 23:00:32 +0200

    refactor

--- src/main/scala/catslaboratory/ApplicationCats.scala
+++ src/main/scala/catslaboratory/ApplicationCats.scala
@@ -7,7 +7,10 @@ import cats.implicits._
 object ApplicationCats extends App {
   println("Hello Cats!".toUpperCase)
 
-  val res: IO[Option[Int]] = IO(Option(3)) <+> IO(None)
+  val f1: Int => IO[Option[Int]] = i => IO(Option(1))
+  val f2: Int => IO[Option[Int]] = i => IO(None)
+
+  val res: IO[Option[Int]] = f2(4) <+> IO(Option(3))
   val r: Option[Int] = res.unsafeRunSync()
   print(r.fold(0)(identity))
 
