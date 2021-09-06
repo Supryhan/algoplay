3c1a76013fc971d80317a7c2c746f61eeecdb915
supryhan <suprigan@gmail.com>
2021-09-07 00:00:34 +0300

    - fix monads approach

--- src/main/scala/catslaboratory/monadslab/Monads.scala
+++ src/main/scala/catslaboratory/monadslab/Monads.scala
@@ -4,7 +4,7 @@ object Monads extends App {
   val aList: List[String] = List("a", "b", "c")
   val bList: List[Int] = List(1, 2, 3)
 
-  def combineLists(List: List[String], bList: List[Int]): List[(String, Int)] =
+  def combineLists(aList: List[String], bList: List[Int]): List[(String, Int)] =
     for {
       a <- aList
       b <- bList
@@ -16,7 +16,8 @@ object Monads extends App {
     def map[A, B](ma: M[A])(f: A => B): M[B] = flatMap(ma)(a => pure(f(a)))
   }
 
-  def combine[M[_], A, B](m1: M[A], m2: M[B])(implicit m: Monad[M]): M[(A, B)] = m.flatMap(m1)(a => m.map(m2)(b => (a, b)))
+  def combine[M[_], A, B](ma: M[A], mb: M[B])(implicit m: Monad[M]): M[(A, B)] =
+    m.flatMap(ma)(a => m.flatMap(mb)(b => m.pure((a, b))))
 
   implicit val listMonad: Monad[List] = new Monad[List] {
     override def pure[A](a: A): List[A] = List(a)
