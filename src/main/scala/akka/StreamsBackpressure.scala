a6fdeb28706b03815ccebf80bb34564ef84cc8c6
supryhan <suprigan@gmail.com>
2021-11-15 10:28:51 +0200

    add promice-future base example

--- src/main/scala/akka/StreamsBackpressure.scala
+++ src/main/scala/akka/StreamsBackpressure.scala
@@ -2,21 +2,30 @@ package akka
 
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
+import akka.stream.OverflowStrategy
 import akka.stream.scaladsl.{Flow, Sink, Source}
+
+import scala.concurrent.Await
+import scala.concurrent.duration.{Duration, MINUTES}
+
 
 object StreamsBackpressure extends App {
 
   implicit val system = ActorSystem(Behaviors.empty, "Streams-Backpressure")
 
   val source = Source(0 to 99)
+
   val flow = Flow[Int].map { i =>
-    Thread.sleep(100)
+    println(s"$i")
     i + 1
   }
-  val sink = Sink.foreach[Int](r => println(s">>>${r.toString}"))
+  val sink = Sink.foreach[Int]{ r =>
+    Thread.sleep(100)
+    println(s">>>${r.toString}")
+  }
 
-  val result = source.via(flow).to(sink)
+  val result = source.via(flow.buffer(16, OverflowStrategy.backpressure)).async.to(sink)
   result.run()
 
-  system.terminate()
+  Await.ready(system.whenTerminated, Duration(1, MINUTES))
 }
