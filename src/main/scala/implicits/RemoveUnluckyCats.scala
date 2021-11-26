6cb311175fc4316e88d0350063cfe061a7fc0c3a
supryhan <suprigan@gmail.com>
2021-11-26 23:12:56 +0200

    - fix implicits

--- src/main/scala/implicits/RemoveUnluckyCats.scala
+++ src/main/scala/implicits/RemoveUnluckyCats.scala
@@ -2,7 +2,18 @@ package implicits
 
 class RemoveUnluckyCats {
   //Write a Truthy typeclass, which allows to call
-  def trueOrNot():Boolean = ??? //function on any type
+  def trueOrNot(): Boolean = ??? //function on any type
+
+  implicit class A[T](value: T) {
+    def trueOrNot(): Boolean = true
+  }
+
+  1.trueOrNot()
+  (0.0).trueOrNot()
+  'a'.trueOrNot()
+  List().trueOrNot()
+  new Exception("no exception").trueOrNot()
+
 
   case class Train(name: String) {
     def is13: Boolean = name.contains("13")
@@ -18,13 +29,57 @@ class RemoveUnluckyCats {
   // TODO: Implement type-class here
 
 
-
-
 //  trains.removeUnlucky      //shoud be List(Train(k2839))
 //  cats.removeUnlucky        //shoud be List(Cat(5,white))
 }
 
-
 class X {
   def removeUnlucky() = ???
+}
+
+object TC extends App {
+  sealed trait Json
+  final case class JsObject(get: Map[String, Json]) extends Json
+  final case class JsString(get: String) extends Json
+  final case class JsNumber(get: Double) extends Json
+  final case object JsNull extends Json
+
+  trait JsonWriter[A] {
+    def write(value: A): Json
+  }
+
+  object JsonWriterInstances {
+    implicit val stringWriter: JsonWriter[String] = value => JsString(value)
+    implicit val personWriter: JsonWriter[Int] = value => JsNumber(value.toDouble)
+    implicit val optionIntWriter: JsonWriter[Option[Int]] = {
+      case Some(value) => JsNull
+      case None => JsNull
+    }
+    implicit val optionPersonWriter: JsonWriter[Option[String]] = {
+      case Some(value) => JsNull
+      case None => JsNull
+    }
+
+    implicit def optionWriter[A](implicit writer: JsonWriter[A]): JsonWriter[Option[A]] = {
+      case Some(aValue) => writer.write(aValue)
+      case None => JsNull
+    }
+  }
+
+  object Json {
+    def toJson[A](value: A)(implicit w: JsonWriter[A]): Json = w.write(value)
+  }
+
+  import JsonWriterInstances._
+
+  Json.toJson(1)
+  Json.toJson("1")
+
+  implicit class JsonWriterOps[A](value: A) {
+    def toJson(implicit w: JsonWriter[A]): Json = w.write(value)
+  }
+
+  1.toJson
+  "1".toJson
+  Option(1).toJson
 }
