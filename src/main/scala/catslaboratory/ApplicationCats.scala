07894eb683f551b9cd5ad012b9e54831bf9ef202
supryhan <suprigan@gmail.com>
2021-12-15 18:55:11 +0200

    - fix combine method

--- src/main/scala/catslaboratory/ApplicationCats.scala
+++ src/main/scala/catslaboratory/ApplicationCats.scala
@@ -1,17 +1,26 @@
 package catslaboratory
 
-import cats._
 import cats.effect.IO
 import cats.implicits._
 
 object ApplicationCats extends App {
   println("Hello Cats!".toUpperCase)
 
-  val f1: Int => IO[Option[Int]] = i => IO(Option(1))
-  val f2: Int => IO[Option[Int]] = i => IO(None)
+  val testPositive: Int => IO[Option[Int]] = i =>
+    IO {
+      if (i > 0)
+        Some(1)
+      else
+        None
+    }
 
-  val res: IO[Option[Int]] = f2(4) <+> IO(Option(3))
-  val r: Option[Int] = res.unsafeRunSync()
-  print(r.fold(0)(identity))
+  val noneInAnyCase: Int => IO[Option[Int]] = _ => IO(None)
 
+  val combinedOptions1: IO[Option[Int]] = noneInAnyCase(4) <+> IO(Option(3))
+  val resNegative: Option[Int] = combinedOptions1.unsafeRunSync() // None
+  println(resNegative.fold(0)(identity)) // 0 in case of None
+
+  val combinedOptions2: IO[Option[Int]] = testPositive(4) <+> noneInAnyCase(3)
+  val resPositive: Option[Int] = combinedOptions2.unsafeRunSync() // Some
+  println(resPositive.fold(0)(identity))
 }
