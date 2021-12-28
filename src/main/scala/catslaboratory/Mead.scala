6e2e5d9673e5d68005b5bbae6b791c797fad0f14
supryhan <suprigan@gmail.com>
2021-12-29 00:19:25 +0200

    - add Mead

---
+++ src/main/scala/catslaboratory/Mead.scala
@@ -0,0 +1,34 @@
+package catslaboratory
+
+import cats.data.OptionT
+import cats.effect.IO
+import cats.implicits._
+
+object Mead extends App {
+
+  case class Mead(name: String, honeyRatio: Double, agingYears: Double)
+
+  type Query[T] = IO[Option[T]]
+
+  def defineMead(qName: Query[String],
+                 qHoneyRatio: Query[Double],
+                 qAgingYears: Query[Double]): Query[Mead] =
+    (for {
+      name       <- OptionT(qName)
+      honeyRatio <- OptionT(qHoneyRatio)
+      agingYears <- OptionT(qAgingYears)
+    } yield Mead(name, honeyRatio, agingYears)).value
+
+  def defineMead2(qName: Query[String],
+                  qHoneyRatio: Query[Double],
+                  qAgingYears: Query[Double]): Query[Mead] =
+    for {
+      name       <- qName
+      honeyRatio <- qHoneyRatio
+      agingYears <- qAgingYears
+    } yield (name, honeyRatio, agingYears).mapN(Mead)
+
+  println(defineMead(IO("hello".some), IO(Option(0.1)), IO(Option(1.0))).unsafeRunSync().fold("error")(_.toString))
+  println(defineMead2(IO("hello".some), IO(Option(0.1)), IO(Option(1.0))).unsafeRunSync().fold("error")(_.toString))
+
+}
