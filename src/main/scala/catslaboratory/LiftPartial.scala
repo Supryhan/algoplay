527c2a8f7554158becba2dcf3b1cc3adb2b86f0a
supryhan <suprigan@gmail.com>
2021-12-30 17:00:16 +0200

    - add ValidatedApproach

--- src/main/scala/catslaboratory/LiftPartial.scala
+++ src/main/scala/catslaboratory/LiftPartial.scala
@@ -13,4 +13,15 @@ object LiftPartial extends App {
 
   println(intMatcher(1))
   // println(intMatcher(0)) - Exception scala.MatchError
+
+  import cats.implicits._
+//  val o1: Option[Int] = none
+//  val o2: Option[Int] = 2.some
+
+  val res1 = { println("o11:"); none }.flatMap(_ => { val e = 1 / 0; println("o21"); 2.some })
+  val res2 = { println("o12:"); none } *> { /*val e = 1 / 0;*/ println("o22"); 2.some }
+
+  println(s"flatMap result: $res1")
+  println(s"*> result: $res2")
+
 }
