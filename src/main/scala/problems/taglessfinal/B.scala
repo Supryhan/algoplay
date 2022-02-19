6dd615b478c1bce9b98e1213c1d20dfb987171d6
supryhan <suprigan@gmail.com>
2022-02-19 13:49:03 +0200

    - tagless final

--- src/main/scala/problems/taglessfinal/B.scala
+++ src/main/scala/problems/taglessfinal/B.scala
@@ -11,28 +11,39 @@ object B extends App {
   }
   println(task1(12))
 
+  type Error = String
+  type EitherError[A] = Either[Error, A]
+
+  val userStorage: Set[Int] = (1 to 99).toSet
+
   case class UserId(value: Int)
 
   trait UserRepo[F[_]] {
-    def getUser(userId: Option[Int]): F[UserId]
+    def getUser(id: Int): F[UserId]
   }
 
   object UserRepo {
     def apply[F[_]](implicit UserR: UserRepo[F]): UserRepo[F] = UserR
   }
-  def getUser[F[_]: UserRepo](userId: Option[Int]): F[UserId] =
-    UserRepo[F].getUser(userId)
+  def getUser[F[_]: UserRepo](id: Int): F[UserId] =
+    UserRepo[F].getUser(id)
 
   implicit object OptUserRepo extends UserRepo[Option] {
-    def getUser(userId: Option[Int]): Option[UserId] = Some(UserId(1331))
+    def getUser(id: Int): Option[UserId] = Option.when(userStorage.contains(id))(UserId(id))
   }
   implicit object ListUserRepo extends UserRepo[List] {
-    def getUser(userId: Option[Int]): List[UserId] = List(UserId(1331))
+    def getUser(id: Int): List[UserId] = Option.when(userStorage(id))(UserId(id)).toList
   }
-//  println(UserRepo[Option].getUser(Some(11)))
-  val res: Option[UserId] = getUser[Option](Some(11))
-  val res2: List[UserId] = getUser[List](Some(11))
-  println(res)
+  implicit object EitherUserRepo extends UserRepo[EitherError] {
+    def getUser(id: Int): EitherError[UserId] = Either.cond(userStorage(id), UserId(id), "Can't find user in repo.")
+  }
+//  println(UserRepo[Option].getUser(11))
+  val res1: Option[UserId] = getUser[Option](11) // Some(UserId(11))
+  val res2: List[UserId] = getUser[List](100) // List()
+  val res3: EitherError[UserId] = getUser[EitherError](0) // Can't find user in repo.
+  res1.foreach(println)
+  res2.foreach(println)
+  res3.fold(e => println(e), r => println(r))
 
 }
 
