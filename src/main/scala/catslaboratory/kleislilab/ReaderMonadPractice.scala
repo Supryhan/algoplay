ba191f37fcf38cb33a7465ecdcc1c61d83016068
Supryhan <suprigan@gmail.com>
2022-07-23 20:07:13 +0300

    State Monad Practice

--- src/main/scala/catslaboratory/kleislilab/ReaderMonadPractice.scala
+++ src/main/scala/catslaboratory/kleislilab/ReaderMonadPractice.scala
@@ -7,7 +7,8 @@ object ReaderMonadPractice extends App {
   type Name = String
   case class Bread(name: String)
   case class Cat(breed: Bread, name: Name)
-  val cat1 = ReaderT[IO, Cat, Name]{cat => IO.pure(cat.name)}
-  val result = cat1.andThen(name => IO.pure(name.toUpperCase())).run(Cat(Bread("Otocolobus"), "garfield"))
+  val catReader: ReaderT[IO, Cat, Name] = ReaderT[IO, Cat, Name]{ cat => IO.pure(cat.name)}
+  val catReaderUppercase: ReaderT[IO, Cat, Name] = catReader.andThen(name => IO.pure(name.toUpperCase()))
+  val result = catReaderUppercase.run(Cat(Bread("Otocolobus"), "garfield"))
   println(result.unsafeRunSync())
 }
