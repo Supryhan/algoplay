378f342a9511f35f41189c603dbfad6ab4156e12
Supryhan <suprigan@gmail.com>
2022-09-10 21:35:04 +0300

    practice new type

--- src/main/scala/problems/refined/CoerceFeature.scala
+++ src/main/scala/problems/refined/CoerceFeature.scala
@@ -2,10 +2,9 @@ package problems.refined
 
 import cats.data.{EitherNel, ValidatedNel}
 import cats.implicits._
-import eu.timepit.refined.api.Refined
+import eu.timepit.refined.api.{Refined, RefinedTypeOps, Validate}
 import eu.timepit.refined.collection.{Contains, Empty}
 import io.estatico.newtype.macros._
-import eu.timepit.refined.api.RefinedTypeOps
 import eu.timepit.refined.numeric.Greater
 import eu.timepit.refined.predicates.all.NonEmpty
 import eu.timepit.refined.refineV
@@ -22,7 +21,9 @@ object CoerceFeature extends App {
   println(s"Refine result 3: ${refineV[Empty]("")}")
 
   type UsernameFtp = String Refined Contains['g']
-  @newtype case class Email(value: String)
+  type EmailR = NonEmptyString
+  //  object EmailR extends RefinedTypeOps[EmailR, String]
+  @newtype case class Email(value: EmailR)
   case class User(name: UsernameFtp, email: Email)
   def lookup[F[_]](username: UsernameFtp): F[Option[User]] = ???
 
@@ -42,15 +43,50 @@ object CoerceFeature extends App {
     GTFiveOps.from(b).toValidatedNel
     ).mapN(MyType)
 
-  println(validateNel("name", 42)) // Valid(MyType(name,42))
-  println(validateNel("", 4)) // Invalid(NonEmptyList(Predicate isEmpty() did not fail., Predicate failed: (4 > 5).))
+  println(s"Refine result 7: ${validateNel("name", 42)}") // Valid(MyType(name,42))
+  println(s"Refine result 8: ${validateNel("", 4)}") // Invalid(NonEmptyList(Predicate isEmpty() did not fail., Predicate failed: (4 > 5).))
 
   def validateEitherNel(a: String, b: Int): EitherNel[String, MyType] = (
     NonEmptyString.from(a).toEitherNel, // Either[String, NonEmptyString] => EitherNel[String, NonEmptyString]
     GTFiveOps.from(b).toEitherNel
     ).parMapN(MyType)
 
-  println(validateEitherNel("name", 42)) // Right(MyType(name,42))
-  println(validateEitherNel("", 4)) // Left(NonEmptyList(Predicate isEmpty() did not fail., Predicate failed: (4 > 5).))
+  println(s"Refine result 9: ${validateEitherNel("name", 42)}") // Right(MyType(name,42))
+  println(s"Refine result 10: ${validateEitherNel("", 4)}") // Left(NonEmptyList(Predicate isEmpty() did not fail., Predicate failed: (4 > 5).))
 
+  type UserNameR = NonEmptyString
+  object UserNameR extends RefinedTypeOps[UserNameR, String]
+  type NameR = NonEmptyString
+  object NameR extends RefinedTypeOps[NameR, String]
+//  type EmailR = String Refined NonEmpty
+
+  @newtype case class UserName(value: UserNameR)
+  @newtype case class Name(value: NameR)
+  case class Person(username: UserName, name: Name, email: Email)
+
+  import NewtypeRefinedOps._
+  import io.estatico.newtype.ops._
+  def mkPerson(u: String, n: String, e: String): EitherNel[String, Person] =
+    (validate[UserName](u),
+      validate[Name](n),
+      validate[Email](e)).parMapN(Person.apply)
+
+  println(s"Refine result 11: ${mkPerson("username", "name", "email")}")
+  println(s"Refine result 12: ${mkPerson("", "", "email")}")
+}
+
+object NewtypeRefinedOps {
+
+  import io.estatico.newtype.Coercible
+  import io.estatico.newtype.ops._
+
+  final class NewtypeRefinedPartiallyApplied[A] {
+    def apply[T, P](raw: T)(implicit
+                            c: Coercible[Refined[T, P], A],
+                            v: Validate[T, P]): EitherNel[String, A] =
+      refineV[P](raw).toEitherNel.map(_.coerce[A])
+  }
+
+  def validate[A]: NewtypeRefinedPartiallyApplied[A] =
+    new NewtypeRefinedPartiallyApplied[A]
 }
