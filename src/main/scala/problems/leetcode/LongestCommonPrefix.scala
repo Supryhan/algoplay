0cd6c0d40121d2cb78881a8687fe47ae40ea9d96
Supryhan <suprigan@gmail.com>
2023-02-15 23:54:38 +0200

    add solution for longest prefix

---
+++ src/main/scala/problems/leetcode/LongestCommonPrefix.scala
@@ -0,0 +1,19 @@
+package problems.leetcode
+
+object LongestCommonPrefix {
+  def longestCommonPrefix(strs: Array[String]): String = {
+    if(strs.isEmpty) return ""
+    strs.foldLeft(strs.head){(acc, s) =>
+      findSimilar(acc, s)
+    }
+  }
+
+  def findSimilar(prefix: String, s: String): String = {
+    if(prefix == "")
+      ""
+    else if(s.indexOf(prefix) != 0)
+      findSimilar(prefix.dropRight(1), s)
+    else
+      prefix
+  }
+}
