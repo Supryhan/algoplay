6d67af3711f61fe73574be690a80e15f05b81e97
Supryhan <suprigan@gmail.com>
2023-06-29 01:56:54 +0300

    search last words length

--- src/main/scala/problems/leetcode/P58LengthOfLastWordJ.java
+++ src/main/scala/problems/leetcode/P58LengthOfLastWordJ.java
@@ -7,6 +7,7 @@ public class P58LengthOfLastWordJ {
         System.out.println("1: " + problem.lengthOfLastWord("Hello World"));
         System.out.println("2: " + problem.lengthOfLastWord("   fly me   to   the moon  "));
         System.out.println("3: " + problem.lengthOfLastWord("luffy is still joyboy"));
+        System.out.println("4: " + problem.lengthOfLastWord("a"));
     }
 
     public int lengthOfLastWord(String s) {
@@ -19,9 +20,11 @@ public class P58LengthOfLastWordJ {
                 if(' ' != arr[pointer] && !flag) {
                     first = pointer;
                     flag = true;
-                } else if(pointer == arr.length - 1 && flag) {
+                }
+                if(pointer == arr.length - 1 && flag) {
                     last = pointer + 1;
-                } else if(' ' == arr[pointer] && flag) {
+                }
+                if(' ' == arr[pointer] && flag) {
                     last = pointer;
                     flag = false;
                 }
