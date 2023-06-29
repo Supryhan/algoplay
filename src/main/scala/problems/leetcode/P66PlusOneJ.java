4d1a92e8d484699a92f1f78f15bc372c40d61552
Supryhan <suprigan@gmail.com>
2023-06-29 16:56:29 +0300

    increment value

---
+++ src/main/scala/problems/leetcode/P66PlusOneJ.java
@@ -0,0 +1,34 @@
+package problems.leetcode;
+
+import java.util.Arrays;
+
+import static java.lang.System.arraycopy;
+
+public class P66PlusOneJ {
+    public static void main(String[] args) {
+        P66PlusOneJ problem = new P66PlusOneJ();
+        System.out.println("1: " + Arrays.toString(problem.plusOne(new int[]{1, 2, 3, 4, 5})));
+        System.out.println("2: " + Arrays.toString(problem.plusOne(new int[]{9})));
+    }
+
+    public int[] plusOne(int[] digits) {
+        int length = digits.length;
+        boolean flag = true;
+
+        for(int i = length - 1; i >= 0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                flag = false;
+                break;
+            }
+            digits[i] = 0;
+        }
+        if(flag) {
+            int[] c = new int[length + 1];
+            arraycopy(digits, 0, c, 1, length);
+            c[0] = 1;
+            return c;
+        }
+        return digits;
+    }
+}
