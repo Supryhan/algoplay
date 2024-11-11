3700d5fbd8f4485c1760ff5341df0350340be053
Vitalii Supryhan <suprigan@gmail.com>
2024-11-11 23:06:37 +0200

    update: refactor quicksort

--- src/main/scala/algo/sorting/QuickSort.scala
+++ src/main/scala/algo/sorting/QuickSort.scala
@@ -2,24 +2,28 @@ package algo.sorting
 
 object QuickSort extends App {
 
-  def quicksort[A: Ordering](ls: List[A]): List[Any] = {
+  def quicksort[A: Ordering](ls: List[A]): List[A] = {
     import Ordered._
 
     def sort(ls: List[A])(parent: List[A]): List[A] = {
-      if (ls.size <= 1) ls ::: parent else {
+      if (ls.size <= 1) ls ::: parent
+      else {
         val pivot = ls.head
-        val (less, equal, greater) = ls
-          .foldLeft((List[A](), List[A](), List[A]())) {
-            case ((less, equal, greater), e) =>
-              if (e < pivot) (e :: less, equal, greater)
-              else if (e == pivot) (less, e :: equal, greater)
-              else (less, equal, e :: greater)
-          }
+        val (less, equal, greater) = ls.foldLeft((List[A](), List[A](), List[A]())) {
+          case ((less, equal, greater), e) =>
+            if (e < pivot) (e :: less, equal, greater)
+            else if (e == pivot) (less, e :: equal, greater)
+            else (less, equal, e :: greater)
+        }
         sort(less)(equal ::: sort(greater)(parent))
       }
     }
+
     sort(ls)(Nil)
   }
 
-  println(s"Sorted list: ${quicksort(List(12, 56, 1, 2, 3, 4, 5, 6, 7, 0))}")
+
+  val unsortedList = List(3, 6, 8, 10, 1, 2, 1, 12, 56, 1, 2, 3, 4, 5, 6, 7, 0)
+  val sortedList = quicksort(unsortedList)
+  println(sortedList)
 }
