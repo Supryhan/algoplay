c8598cc77d70867ddf7aac1917fdddb84e7b9f1b
Vitalii Supryhan <suprigan@gmail.com>
2025-01-13 23:52:36 +0200

    update: add Monoid practicing

---
+++ src/main/scala/catslaboratory/iopractice/SemigroupNMonoidPractice.scala
@@ -0,0 +1,22 @@
+package catslaboratory.iopractice
+
+import cats.Monoid
+
+object SemigroupNMonoidPractice extends App {
+  case class ShoppingCard(items: List[String], total: BigDecimal)
+
+  implicit object ShoppingCardMonoid extends Monoid[ShoppingCard] {
+    override def empty: ShoppingCard = ShoppingCard(List.empty[String], BigDecimal(0))
+
+    override def combine(x: ShoppingCard, y: ShoppingCard): ShoppingCard = ShoppingCard(y.items ::: x.items, y.total + x.total)
+  }
+
+  def reduceShoppingCard(cards: List[ShoppingCard])(implicit sc: Monoid[ShoppingCard]): Double = cards.foldLeft(sc.empty)(sc.combine).total.doubleValue
+
+  println(
+    s"""
+       |My shoppings:
+       |${reduceShoppingCard(List(ShoppingCard(List("1", "2", "3"), 123), ShoppingCard(List("4", "5", "6"), 456)))}
+       |""".stripMargin)
+
+}
