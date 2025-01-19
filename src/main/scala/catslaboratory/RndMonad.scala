a8c145e0e89ffb07c897211a7328b780d3d9b9d8
Vitalii Supryhan <suprigan@gmail.com>
2025-01-19 13:57:06 +0200

    update: update rnd monad and its laws

--- src/main/scala/catslaboratory/RndMonad.scala
+++ src/main/scala/catslaboratory/RndMonad.scala
@@ -1,6 +1,7 @@
 package catslaboratory
 
 import cats.Monad
+import cats.implicits.toFlatMapOps
 
 object RndMonad extends App {
 
@@ -20,15 +21,52 @@ object RndMonad extends App {
       }
 
     override def tailRecM[A, B](a: A)(f: A => Rnd[Either[A, B]]): Rnd[B] = {
-      val e: Random => Either[A, B] = f(a).run
       Rnd { random: Random =>
+        val e: Random => Either[A, B] = f(a).run
         val e1: Either[A, B] = e(random)
         e1 match {
           case Left(value) => tailRecM(value)(f).run(random)
-          case Right(value) => value
         }
       }
     }
+  }
+
+  val intToRndDouble: Int => Rnd[Double] = (x: Int) => Rnd(_ => x.toDouble)
+  val doubleToRndString: Double => Rnd[String] = (x: Double) => Rnd(_ => x.toString)
+  val m: Rnd[Int] = Monad[Rnd].pure(1)
+  val randomOne = new Random(1)
+
+  // Monad laws:
+  // 1. Left id
+  assert {
+    Monad[Rnd].pure(1)
+      .flatMap(intToRndDouble).run(randomOne) == intToRndDouble(1).run(randomOne)
+  }
+  //  assert(Monad[Rnd].pure(1).flatMap(intToListDouble) == intToListDouble(1))
+
+  // 2. Right id
+  assert {
+    m.flatMap {
+      e =>
+        Monad[Rnd].pure(e)
+    }.run(randomOne) == m.run(randomOne)
+  }
+
+  // 3. Associativity
+  assert {
+    m
+      .flatMap(intToRndDouble)
+      .flatMap(doubleToRndString)
+      .run(randomOne) ==
+      m
+        .flatMap {
+          x =>
+            intToRndDouble(x)
+              .flatMap {
+                y =>
+                  doubleToRndString(y)
+              }
+        }.run(randomOne)
   }
 
 }
