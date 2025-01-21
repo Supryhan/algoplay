2696354ecc857348e189eaa4c834ac8e798e51db
Vitalii Supryhan <suprigan@gmail.com>
2025-01-21 21:28:03 +0200

    update: refactor mergesort

--- src/main/scala/catslaboratory/problems/MergeSort.scala
+++ src/main/scala/catslaboratory/problems/MergeSort.scala
@@ -2,25 +2,25 @@ package catslaboratory.problems
 
 object MergeSort extends App {
 
-  def msort[T](less: (T, T) => Boolean)(xs: List[T]): List[T] = {
+  def msort[T](cond: (T, T) => Boolean)(xs: List[T]): List[T] = {
     def merge(xs: List[T], ys: List[T]): List[T] = (xs, ys) match {
       case (Nil, _) => ys
       case (_, Nil) => xs
       case (x :: xs1, y :: ys1) =>
-        if (less(x, y)) x :: merge(xs1, ys)
+        if (cond(x, y)) x :: merge(xs1, ys)
         else y :: merge(xs, ys1)
     }
 
     val n = xs.length / 2
     if (n == 0) xs
     else {
-      val (ys, zs) = xs splitAt n
-      merge(msort(less)(ys), msort(less)(zs))
+      val (left, right) = xs splitAt n
+      merge(msort(cond)(left), msort(cond)(right))
     }
   }
 
-  private val sordIntAsc = msort((x: Int, y: Int) => x < y) _
-  private val result = sordIntAsc(9 :: 1 :: 8 :: 3 :: 2 :: Nil)
+  private val mergeSortIntAsc = msort((x: Int, y: Int) => x < y) _
+  private val result = mergeSortIntAsc(9 :: 1 :: 8 :: 3 :: 2 :: Nil)
   println(result.toString)
 
 }
