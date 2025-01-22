f88198ce7231956e4c1e090ce2e6a6cdae8b4c63
Vitalii Supryhan <suprigan@gmail.com>
2025-01-22 14:35:35 +0200

    update: update StateMonad operators

--- src/main/scala/catslaboratory/monadslab/StateMonadPractice.scala
+++ src/main/scala/catslaboratory/monadslab/StateMonadPractice.scala
@@ -1,10 +1,11 @@
 package catslaboratory.monadslab
 
+import cats.data.State
+
 object StateMonadPractice extends App {
 
   case class ShoppingCart(items: List[String], total: Double)
 
-  import cats.data.State
 
   def addToCart(item: String, price: Double): State[ShoppingCart, Double] = State {
     (cart: ShoppingCart) =>
@@ -20,5 +21,45 @@ object StateMonadPractice extends App {
   private val actualResult = s"Total: ${shoppingResult.run(ShoppingCart(Nil, 0)).value._2}"
   assert("Total: 210.0" == actualResult)
   println(actualResult)
+
+  // returns a State data structure that, when run, will not change the state but will issue the value f(a)
+  def inspect[A, B](f: A => B): State[A, B] = State(a => (a, f(a)))
+
+  val expectedI = s"Inspect: ${inspect[String, String](a => a).run("x").value.toString}"
+  assert("Inspect: (x,x)" == expectedI)
+  println(expectedI)
+
+  // returns a State data structure that, when run, returns the value of that state and makes no changes
+  def get[A]: State[A, A] = State(a => (a, a))
+
+  val expectedG = s"Get: ${get[String].run("x").value._2}"
+  assert("Get: x" == expectedG)
+  println(expectedG)
+
+  // returns a State data structure that, when run, returns Unit and sets the state to that value
+  def set[A](value: A): State[A, Unit] = State(_ => (value, ()))
+
+  val expectedS = s"Set: ${get[String].run("x").value._1}"
+  assert("Set: x" == expectedS)
+  println(expectedS)
+
+  // returns a State data structure that, when run, will return Unit and sets the state to f(state)
+  def modify[A](f: A => A): State[A, Unit] = State(a => (f(a), ()))
+
+  val expectedU = s"Modify: ${modify[String](a => a).run("x").value._2.toString}"
+  assert("Modify: ()" == expectedU)
+  println(expectedU)
+
+  // PROGRAM:
+  private val program: State[Int, (Int, Int, String, Int)] = for {
+    _ <- set[Int](1)
+    s1 <- get[Int]
+    _ <- modify[Int](_ + 41)
+    s2 <- get[Int]
+    s3 <- inspect[Int, String](arg => s"`Hello: $arg`")
+    s4 <- get[Int]
+  } yield (s1, s2, s3, s4)
+
+  println(s"Program: ${program.run(0).value._2.toString}")
 
 }
