33bab01d45a28531f751f340d3ab9468cbe4dc9c
Vitalii Supryhan <suprigan@gmail.com>
2025-02-09 13:40:59 +0200

    update: refactor pure config

--- src/main/scala/problemslab/PureConfigExamples.scala
+++ src/main/scala/problemslab/PureConfigExamples.scala
@@ -9,10 +9,10 @@ import java.util.UUID
 object PureConfigExamples extends App {
   // VM options: -Dconfig.resource=local/env.conf
   val source: Result[ServiceConf] = ConfigSource.default.load[ServiceConf]
-//  val source: Result[ServiceConf] = ConfigSource.defaultApplication.load[ServiceConf]
-  println(s"Config: ${getConf(source).toString}")
+  //  val source: Result[ServiceConf] = ConfigSource.defaultApplication.load[ServiceConf]
+  println(s"Config: ${getConf(source).iterator.mkString}")
 
-//  implicit val myIntReader: ConfigReader[Number] = ConfigReader[Int].map(n => Number(Id(n)))
+  //  implicit val myIntReader: ConfigReader[Number] = ConfigReader[Int].map(n => Number(Id(n)))
 
   def getConf(source: Result[ServiceConf]): Option[ServiceConf] = source match {
     case Right(value) => Some(value)
@@ -21,12 +21,10 @@ object PureConfigExamples extends App {
 }
 
 case class Port(number: Int) extends AnyVal
-
 sealed trait AuthMethod
 case class Login(username: String, password: String) extends AuthMethod
 case class Token(token: UUID) extends AuthMethod
 case class PrivateKey(pkFile: java.io.File) extends AuthMethod
-
 case class ServiceConf(
                         env: String,
                         host: String,
